a1717e009cafbbc5d963e85e884032725067faf5
faresomarr506-star <faresomarr506@gmail.com>
2026-08-21 23:19:05 +0300

    Set up Express proxy server in server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/', createProxyMiddleware({
+    target: 'http://147.135.213.131:20046',
+    changeOrigin: true,
+    ws: true,
+    secure: false
+}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Proxy server running on port ${PORT}`);
+});
